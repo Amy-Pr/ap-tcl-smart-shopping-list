c781fe619472a6ace073a562eaf7ced6a5471b5f
amy_pr <ampreci@gmail.com>
2023-06-20 18:20:24 -0700

    remove comment

--- src/api/config.js
+++ src/api/config.js
@@ -1,17 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-//Original configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCp5Q7injiGTEKhPMBvq25n2CYzRegXPbQ",
-//   authDomain: "tcl-57-smart-shopping-list.firebaseapp.com",
-//   projectId: "tcl-57-smart-shopping-list",
-//   storageBucket: "tcl-57-smart-shopping-list.appspot.com",
-//   messagingSenderId: "218407207047",
-//   appId: "1:218407207047:web:febc6b43d7acb01f43f822"
-// };
-
 // My configuration
 const firebaseConfig = {
 	apiKey: 'AIzaSyC2gYe75Qnno_rwBsWMNtuBqoC5hZrilFg',
